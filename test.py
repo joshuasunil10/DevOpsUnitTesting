8c399f9080f693cf5ac1d8421a1136498971d722
joshuasunil10 <joshuasunilmathew10@gmail.com>
2024-12-02 23:44:29 +0000

    changes

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
-from DevOpsUnitTesting.sudokusolver import solve_sudoku
+
+from sudokusolver import solve_sudoku
 
 class TestSudokuSolver(unittest.TestCase):
 
@@ -61,20 +62,22 @@ class TestSudokuSolver(unittest.TestCase):
         with self.assertRaises(IndexError):
             solve_sudoku(invalid_board)
 
+
     def test_invalid_board_characters(self):
         invalid_board = [
-            [5, 3, 4, 6, 7, 8, 9, 1, 2],
-            [6, 7, 2, 1, 'X', 5, 3, 4, 8],  # Invalid character
-            [1, 9, 8, 3, 4, 2, 5, 6, 7],
-            [8, 5, 9, 7, 6, 1, 4, 2, 3],
-            [4, 2, 6, 8, 5, 3, 7, 9, 1],
-            [7, 1, 3, 9, 2, 4, 8, 5, 6],
-            [9, 6, 1, 5, 3, 7, 2, 8, 4],
-            [2, 8, 7, 4, 1, 9, 6, 3, 5],
-            [3, 4, 5, 2, 8, 6, 1, 7, 9]
+                5, 3, "x", 0, 7, 0, 0, 0, 0,
+                6, 0, 0, 1, 9, 5, 0, 0, 0,
+                0, 9, 8, 0, 0, 0, 0, 6, 0,
+                8, 0, 0, 0, 6, 0, 0, 0, 3,
+                4, 0, 0, 8, 0, 3, 0, 0, 1,
+                7, 0, 0, 0, 2, 0, 0, 0, 6,
+                0, 6, 0, 0, 0, 0, 2, 8, 0,
+                0, 0, 0, 4, 1, 9, 0, 0, 5,
+                0, 0, 0, 0, 8, 0, 0, 7, 9
         ]
         with self.assertRaises(TypeError):
             solve_sudoku(invalid_board)
+
 
 if __name__ == "__main__":
     unittest.main()
