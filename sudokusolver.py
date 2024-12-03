fff5fc410f22c9b91ab0f9aab096abbe5e122292
joshuasunil10 <joshuasunilmathew10@gmail.com>
2024-12-03 00:02:00 +0000

    added a sample solution

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,5 +1,5 @@
 def solve_sudoku(board):
-    
+
     def is_valid_move(board, row, col, num):
         for i in range(9):
             if board[row][i] == num or board[i][col] == num:
@@ -29,3 +29,21 @@ def solve_sudoku(board):
     if solve():
         return board
     return None
+
+if __name__ == "__main__":
+        sample_board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+
+        solved_board = solve_sudoku(sample_board)
+        if solved_board:
+            for row in solved_board:
+                print(row)
